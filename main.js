d34d547936959344baeae00eb824fac6820281e0
dhpdx <dhendrix@gmail.com>
2016-03-05 16:13:17 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,3 @@
+var jSkilling = {'salary': 1111258, 'bonus': 5600000, 'total_stock_value': 26093672};
+var kLay = {'salary': 1072321, 'bonus': 7000000, 'total_stock_value': 49110078};
+var jBaxter = {'salary': 267102, 'bonus': 1200000, 'total_stock_value': 10623258};
