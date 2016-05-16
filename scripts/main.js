7166bc783dd2f6a2e8b32a7e81abbfcafc85776a
Danny Hendrix <danhendrix@gmail.com>
2016-05-16 16:27:17 -0700

    Making Progress, need to get array of data

--- scripts/main.js
+++ scripts/main.js
@@ -1,52 +1,53 @@
-//var unirest = require('unirest');
-
 function movieSearch(title, year){
 	var url = "http://www.omdbapi.com/?s=" + title + "&page=1&r=JSON&y=" + year;
 	return new Promise(function(resolve,reject){
-		$.getJSON(url)
+	$.getJSON(url)
 	.done(function(data){
-		//console.log(formatSearch(data))
 		resolve(formatSearch(data));
 	})
 	.fail(function(err){
 		reject(err);
 		console.log("Failed",err);
 	});
-	//return formatSearch([{"Title":"Batman Begins","Year":"2005","imdbID":"tt0372784","Type":"movie","Poster":"http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg"},{"Title":"Batman","Year":"1989","imdbID":"tt0096895","Type":"movie","Poster":"http://ia.media-imdb.com/images/M/MV5BMTYwNjAyODIyMF5BMl5BanBnXkFtZTYwNDMwMDk2._V1_SX300.jpg"},{"Title":"Batman v Superman: Dawn of Justice","Year":"2016","imdbID":"tt2975590","Type":"movie","Poster":"http://ia.media-imdb.com/images/M/MV5BNTE5NzU3MTYzOF5BMl5BanBnXkFtZTgwNTM5NjQxODE@._V1_SX300.jpg"},{"Title":"Batman Returns","Year":"1992","imdbID":"tt0103776","Type":"movie","Poster":"http://ia.media-imdb.com/images/M/MV5BODM2OTc0Njg2OF5BMl5BanBnXkFtZTgwMDA4NjQxMTE@._V1_SX300.jpg"},{"Title":"Batman Forever","Year":"1995","imdbID":"tt0112462","Type":"movie","Poster":"http://ia.media-imdb.com/images/M/MV5BMjAwOTEyNjg0M15BMl5BanBnXkFtZTYwODQyMTI5._V1_SX300.jpg"},{"Title":"Batman & Robin","Year":"1997","imdbID":"tt0118688","Type":"movie","Poster":"http://ia.media-imdb.com/images/M/MV5BNTM1NTIyNjkwM15BMl5BanBnXkFtZTcwODkxOTQxMQ@@._V1_SX300.jpg"},{"Title":"Batman: The Animated Series","Year":"1992–1995","imdbID":"tt0103359","Type":"series","Poster":"http://ia.media-imdb.com/images/M/MV5BMTU3MjcwNzY3NF5BMl5BanBnXkFtZTYwNzA2MTI5._V1_SX300.jpg"},{"Title":"Batman: Under the Red Hood","Year":"2010","imdbID":"tt1569923","Type":"movie","Poster":"http://ia.media-imdb.com/images/M/MV5BMTMwNDEyMjExOF5BMl5BanBnXkFtZTcwMzU4MDU0Mw@@._V1_SX300.jpg"},{"Title":"Batman: The Dark Knight Returns, Part 1","Year":"2012","imdbID":"tt2313197","Type":"movie","Poster":"http://ia.media-imdb.com/images/M/MV5BMzIxMDkxNDM2M15BMl5BanBnXkFtZTcwMDA5ODY1OQ@@._V1_SX300.jpg"},{"Title":"Batman: Mask of the Phantasm","Year":"1993","imdbID":"tt0106364","Type":"movie","Poster":"http://ia.media-imdb.com/images/M/MV5BMTMzODU0NTYxN15BMl5BanBnXkFtZTcwNDUxNzUyMQ@@._V1_SX300.jpg"}])
 })
 }
 
 function formatSearch(data){
 	var searchData = data.Search.map(function(item){
-		return {
-			imdbID: item.imdbID
-		}
+		return item.imdbID;
 	})
-	return searchData
+	return searchData;
 };
+var mySearch = movieSearch("Apollo","")
 
-// var mySearch = Promise.resolve(movieSearch("Batman",""))
-	movieSearch("Batman","")
-	.then(function(data){
-		console.log(data);
-	})
+mySearch.then(function(data){
+	console.log(data);
+})
 
-// var results = mySearch.then(function(d){return d});
 
-// console.log(results)
+var listOfIds = ["tt0372784", "tt0096895"]
+//console.log(getMovieData(listOfIds))
+
+var newSearch = getMovieData(listOfIds)
 
 function getMovieData(idList){
-	results = [];
-	var url = 'http://www.omdbapi.com/?i='+idList+'&plot=short&r=json';
+	resultsList = [];
 	for(i in idList){
+		var results = new Promise(function(resolve,reject){
+		var url = 'http://www.omdbapi.com/?i='+idList[i]+'&plot=short&r=json';
 		$.getJSON(url)
 		.done(function(data){
-			results.push(data);
-			console.log(results);
+			debugger;
+			resultsList.push(resolve(data));
+			console.log(resultsList);
+			console.log(data);
+			})
 		})
 	}
-	return results;
-};
+	
+	return resultsList;
+}
+
 
 function formatMovieData(data){
 	var movieData = data.map(function(item){
